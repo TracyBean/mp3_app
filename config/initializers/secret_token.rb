51317025ae2d86848ebee9fb9e558b606e3ddae0
TracyBean <tmoscato@riseup.net>
2013-08-22 23:09:01 -0400

    added torrent and delete

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mp3App::Application.config.secret_token = 'a579b6a7123bc2f4da27049a02484539338c55076c57bb5e2325c11abc29c4f9fc3cf908130344a262f3236b48f16c8d187d0733e64b9ebc06fcbb726a5c34b5'
